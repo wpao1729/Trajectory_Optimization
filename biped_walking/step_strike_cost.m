bb66633610dd89a3e04836f619a4ae1ed8737857
wpao1729 <wpao6@gatech.edu>
2021-11-02 19:07:42 -0400

    Delete walking example.

--- biped_walking/step_strike_cost.m
+++
@@ -1,9 +0,0 @@
-function cost = step_strike_cost(var_list,n,tf)
-
-h=tf/(n-1);
-u_list=var_list((55*n+1):(59*n));
-u_mat=reshape(u_list,4,n);
-effort_list=sum(u_mat.^2);
-cost=h/2*(effort_list(1)+effort_list(end)+2*sum(effort_list(2:(end-1))));
-
-end
